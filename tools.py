529a9d5a46924167419791af416ee9e67943df82
oluwafemi oloye <88673316+femi-oloye@users.noreply.github.com>
2025-06-18 15:02:21 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,10 +1,9 @@
 # tools.py
-
 from langchain.agents import Tool
 from airtable_utils import get_customer_info
 
 airtable_tool = Tool.from_function(
     name="get_customer_info",
-    description="Lookup customer support info using email address",
+    description="Fetch customer support details from Airtable using email address",
     func=get_customer_info
 )
